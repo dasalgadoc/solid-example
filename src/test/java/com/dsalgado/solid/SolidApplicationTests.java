36867e1e3b34fa85560a06e8295a02167598ae01
Diego Salgado <dasalgadoc@unal.edu.co>
2023-03-11 15:43:06 -0500

    add base

---
+++ src/test/java/com/dsalgado/solid/SolidApplicationTests.java
@@ -0,0 +1,11 @@
+package com.dsalgado.solid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
